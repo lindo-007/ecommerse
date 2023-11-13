da5fd05e9764340c65ee78df360e16482d578475
lindokuhle shabalala <lindokuhles@lottostar.co.za>
2023-11-13 17:08:54 +0200

    removed crossorigin from head

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto:wght@100;400&display=swap"
           rel="stylesheet"
