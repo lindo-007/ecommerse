cf9c20846d92066c14587c62ddcf45ddefddbe57
lindokuhle shabalala <lindokuhles@lottostar.co.za>
2023-11-15 17:01:45 +0200

    styled product page

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -40,8 +40,8 @@ export const getStaticProps = async ({ params }) => {
 //make global price
 function ProductPage({ product }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <section className="w-11/12 my-12 flex flex-col justify-around md:flex-row">
-        <figure className="flex items-center justify-center my-5" >
+    <section className="w-11/12 flex flex-col justify-around md:flex-row md:items-center">
+        <figure className="flex items-center justify-center my-5 md:flex-1" >
           <Image
             src={product.image}
             width={200}
@@ -49,7 +49,7 @@ function ProductPage({ product }: InferGetStaticPropsType<typeof getStaticProps>
             alt={`Product image of ${product.title}`}
           />
         </figure>
-        <figcaption className="flex-2 my-5">
+        <figcaption className="md:flex-1 my-5 ">
           <h2 className="text-3xl text-blue-300 mb-3">{product.title}</h2>
           <p className="text-xl mb-3">{product.category}</p>
           <p className="mb-2">{product.description}</p>
